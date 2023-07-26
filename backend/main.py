4fad7716e6a19b4413806f60c5394dd4d6e7a5e0
Christoph <biedermann.chris@gmail.com>
2023-07-26 12:48:57 -0700

    Generate image with prompt

--- backend/main.py
+++ backend/main.py
@@ -27,12 +27,11 @@ async def test():
     print('success')
 
 @app.get('/generate')
-async def generate():
+async def generate(prompt: str):
     device = torch.device("mps")
     model_id = "runwayml/stable-diffusion-v1-5"
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to(device)
-    prompt = "a photo of an astronaut riding a horse on mars"
     image = pipe(prompt).images[0]  
     image.save("astronaut_rides_horse.png")
     buffer = BytesIO()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,49 @@
-import {Button, Card, CardMedia} from "@mui/material"
-import './App.css';
-import axios from 'axios'
-import { useState } from 'react';
-
+import { Button, Card, CardMedia, TextField, Box, Stack } from "@mui/material";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-
   const [image, setImage] = useState();
+  const [prompt, updatePrompt] = useState();
 
-  const tester = () => {
-    axios.get('http://localhost:8000/test')
-      .then(res => {
-
-      })
-      .catch(err => {
-
-      })
-  }
-
-  const generate = async () => {
-    axios.get('http://localhost:8000/generate')
-      .then(res => {
+  const generate = async (prompt) => {
+    axios
+      .get(`http://localhost:8000/generate/?prompt=${prompt}`)
+      .then((res) => {
         const data = res.data;
         setImage(res.data);
-
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-
-  }
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-      <Button variant="contained" onClick={generate}>Generate Image</Button>
-      <Card>
-      <CardMedia 
-        component="img"
-        height="320"
-        image={`data:image/png;base64,${image}`} />
-      </Card>
-      </header>
+    <div className="app">
+      <header className="header">QR Code Generator</header>
+      <div className="body">
+        <Box className="sidebar">
+          <Stack spacing={2}>
+          <TextField
+            id="prompt"
+            label="Prompt"
+            onChange={(e) => updatePrompt(e.target.value)}
+            variant="outlined"
+          />
+          <Button variant="contained" onClick={(e) => generate(prompt)}>
+            Generate Image
+          </Button>
+          </Stack>
+        </Box>
+        <div className="image-container">
+          <Card>
+            <CardMedia
+              component="img"
+              image={`data:image/png;base64,${image}`}
+            />
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
