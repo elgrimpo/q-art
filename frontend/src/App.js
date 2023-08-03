6321303db9163059b4952d76cbe16a24adbeb788
Christoph <biedermann.chris@gmail.com>
2023-08-03 12:56:26 -0700

    Add TabContext and MyCodes Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,79 +1,49 @@
-import {
-  Avatar,
-  AppBar,
-  Button,
-  Card,
-  CardMedia,
-  Container,
-  IconButton,
-  Menu,
-  MenuItem,
-  TextField,
-  Tab,
-  Tabs,
-  Toolbar,
-  Tooltip,
-  Typography,
-  Box,
-  Stack,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, Tab, Tabs, Toolbar, Box } from "@mui/material";
 import "./App.css";
-import axios from "axios";
-import { useState } from "react";
-import placeholderImage from "./placeholder_image.png";
 import Generate from "./Generate";
 import logo from "./logo.png";
 import * as React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./mui-theme";
-
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import TabContext from "@mui/lab/TabContext";
+import TabPanel from "@mui/lab/TabPanel";
+import MyCodes from "./MyCodes";
 
 function App() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   const [value, setValue] = React.useState(0);
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="app">
-        <Toolbar display="flex" className='header'>
-          <img src={logo} alt="Logo" />
-          <Box className="menu-container">
-            <Tabs value={value} onChange={handleChange} centered>
-              <Tab label="Generate" />
-              <Tab label="My codes" />
-              <Tab label="Explore" />
-            </Tabs>
-          </Box>
-          <Avatar></Avatar>
-        </Toolbar>
-        <div className="body" style={{}}>
-          <Generate />
+      <TabContext value={value}>
+        <div className="app">
+          {/*-------- App Bar --------*/}
+          <Toolbar display="flex" className="header">
+            <img src={logo} alt="Logo" />
+            <Box className="menu-container">
+              <Tabs value={value} onChange={handleChange} centered>
+                <Tab label="Generate" value="1" />
+                <Tab label="My codes" value="2" />
+                <Tab label="Explore" value="3" />
+              </Tabs>
+            </Box>
+            <Avatar></Avatar>
+          </Toolbar>
+
+          {/*-------- App Body --------*/}
+          <div className="body">
+            <TabPanel value="1">
+              <Generate />
+            </TabPanel>
+            <TabPanel value="2">
+              <MyCodes />
+            </TabPanel>
+            <TabPanel value="3">Explore (Placeholder)</TabPanel>
+          </div>
         </div>
-      </div>
+      </TabContext>
     </ThemeProvider>
   );
 }
