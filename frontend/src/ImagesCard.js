7e7f6e3151ac83fdac67abd11926ba875d29cae7
Christoph <biedermann.chris@gmail.com>
2023-08-09 11:18:41 -0700

    Create separate ImageCard component

---
+++ frontend/src/ImagesCard.js
@@ -0,0 +1,177 @@
+import {
+  Card,
+  CardMedia,
+  Grid,
+  Typography,
+  Paper,
+  Backdrop,
+  Stack,
+  IconButton,
+  Skeleton,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import "./App.css";
+import axios from "axios";
+import base64ToBlob from "b64-to-blob";
+import DownloadTwoToneIcon from "@mui/icons-material/DownloadTwoTone";
+import DeleteForeverTwoToneIcon from "@mui/icons-material/DeleteForeverTwoTone";
+import { ActionTypes } from "./reducers";
+import { useImages, useImagesDispatch } from "./AppProvider";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+function ImageCard(props) {
+  const { variant, item, index, onClick } = props;
+  const { userImages } = useImages();
+
+  const dispatch = useImagesDispatch();
+
+  const theme = useTheme();
+  const primaryColor = theme.palette.primary.main;
+
+  // -------- Actions ----------
+  const downloadImage = (image) => {
+    const blob = base64ToBlob(image);
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "my_qr_art.png";
+    link.click();
+  };
+
+  const deleteImage = (id) => {
+    axios
+      .delete(`http://localhost:8000/images/delete/${id}`)
+      .then(() => {
+        if (index > -1) {
+          // Remove image from array
+          const updatedImages = [
+            ...userImages.slice(0, index),
+            ...userImages.slice(index + 1),
+          ];
+
+          // Update UserImages state
+          dispatch({
+            type: ActionTypes.SET_USER_IMAGES,
+            payload: updatedImages,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleCopy = (item) => {
+    const copyValues = {
+      website: item.content,
+      prompt: item.prompt,
+      image_quality: item.image_quality,
+      qr_weight: item.qr_weight,
+      negative_prompt: item.negative_prompt,
+      seed: item.seed,
+    };
+    dispatch({
+      type: ActionTypes.SET_GENERATE_FORM_VALUES,
+      payload: copyValues,
+    });
+    dispatch({
+      type: ActionTypes.SET_GENERATED_IMAGE,
+      payload: item,
+    });
+  };
+
+  return (
+    <Grid item md={2} key={index}>
+      {" "}
+      <Card
+        elevation={0}
+        key={index}
+        sx={{
+          padding: "1.2rem",
+          backgroundColor: primaryColor,
+          borderRadius: "5px",
+        }}
+        color="primary"
+      >
+        {variant == "skeleton" ? (
+          <Skeleton
+            variant="rounded"
+            width="100%"
+            height="0px"
+            animation="wave"
+            sx={{
+              aspectRatio: "1/1",
+              paddingTop: "100%", // Set the height to be the same as the width
+            }}
+            key={index + "_1"}
+          />
+        ) : (
+          <CardMedia
+            component="img"
+            image={`data:image/png;base64,${item?.image_str}`}
+            sx={{ borderRadius: "5px" }}
+            onClick={onClick}
+            key={index}
+          />
+        )}
+
+        {variant == "skeleton" ? (
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={3}
+            sx={{ mt: "1rem" }}
+            key={index + "_2"}
+          >
+            <Skeleton
+              variant="circular"
+              width={30}
+              height={30}
+              key={index + "_3"}
+            />
+            <Skeleton
+              variant="circular"
+              width={30}
+              height={30}
+              key={index + "_4"}
+            />
+            <Skeleton
+              variant="circular"
+              width={30}
+              height={30}
+              key={index + "_5"}
+            />
+          </Stack>
+        ) : (
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={3}
+            sx={{ mt: "1rem" }}
+            key={index + "_6"}
+          >
+            <IconButton
+              onClick={() => downloadImage(item.image_str)}
+              key={index + "_1"}
+            >
+              <DownloadTwoToneIcon key={index}/>
+            </IconButton>
+            <IconButton key={index + "_2"} onClick={() => handleCopy(item)}>
+              <ContentCopyIcon index={index} />
+            </IconButton>
+            <IconButton
+              onClick={() => deleteImage(item._id)}
+              key={index + "_3"}
+            >
+              <DeleteForeverTwoToneIcon key={index} />
+            </IconButton>
+          </Stack>
+        )}
+      </Card>
+    </Grid>
+  );
+}
+
+export default ImageCard;
--- frontend/src/MyCodes.js
+++ frontend/src/MyCodes.js
@@ -1,38 +1,23 @@
 import {
-  Card,
-  CardMedia,
-  CircularProgress,
-  Box,
   Grid,
-  Typography,
   Paper,
   Backdrop,
-  Stack,
   IconButton,
-  Skeleton,
+  List,
+  ListItemText,
 } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import ShareTwoToneIcon from "@mui/icons-material/ShareTwoTone";
 import "./App.css";
 import axios from "axios";
 import { useState, useEffect, useRef } from "react";
-import base64ToBlob from "b64-to-blob";
-import DownloadTwoToneIcon from "@mui/icons-material/DownloadTwoTone";
-import DeleteForeverTwoToneIcon from "@mui/icons-material/DeleteForeverTwoTone";
 import ChevronRightTwoToneIcon from "@mui/icons-material/KeyboardArrowRightTwoTone";
 import ChevronLeftTwoToneIcon from "@mui/icons-material/ChevronLeftTwoTone";
-import dayjs from "dayjs";
 import { ActionTypes } from "./reducers";
 import { useImages, useImagesDispatch } from "./AppProvider";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import ImageCard from "./ImagesCard";
 
 function MyCodes() {
   const dispatch = useImagesDispatch();
   const { userImages, loadingUserImages, userImagesPage } = useImages();
-
-  const theme = useTheme();
-  const primaryColor = theme.palette.primary.main;
-  const secondaryColor = theme.palette.secondary.main;
 
   // ----- Get Images ------ //
   const getMoreImages = () => {
@@ -102,60 +87,6 @@ function MyCodes() {
     };
   }, [loadingUserImages, userImages]);
 
-  // -------- Actions ----------
-  const downloadImage = (image) => {
-    const blob = base64ToBlob(image);
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "my_qr_art.png";
-    link.click();
-  };
-
-  const deleteImage = (id) => {
-    axios
-      .delete(`http://localhost:8000/images/delete/${id}`)
-      .then(() => {
-        const index = userImages.findIndex((image) => image._id === id);
-
-        if (index > -1) {
-          // Remove image from array
-          const updatedImages = [
-            ...userImages.slice(0, index),
-            ...userImages.slice(index + 1),
-          ];
-
-          // Dispatch SET_USER_IMAGES action
-          dispatch({
-            type: ActionTypes.SET_USER_IMAGES,
-            payload: updatedImages,
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const handleCopy = (item) => {
-    const copyValues = {
-      website: item.content,
-      prompt: item.prompt,
-      image_quality: item.image_quality,
-      qr_weight: item.qr_weight,
-      negative_prompt: item.negative_prompt,
-      seed: item.seed,
-    };
-    dispatch({
-      type: ActionTypes.SET_GENERATE_FORM_VALUES,
-      payload: copyValues,
-    });
-    dispatch({
-      type: ActionTypes.SET_GENERATED_IMAGE,
-      payload: item,
-    });
-  };
-
   // --------- Modal ----------
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
   const [open, setOpen] = useState(false);
@@ -185,6 +116,7 @@ function MyCodes() {
 
   return (
     <div>
+      {/*------ Images List ------*/}
       <Grid
         container
         direction="row"
@@ -194,58 +126,23 @@ function MyCodes() {
         spacing={3}
         sx={{ mb: "1.5rem" }}
       >
-        {/*------ Images List ------*/}
-
-        <>
-          {userImages &&
-            userImages.map((item, index) => (
-              <Grid item md={2} key={index}>
-                {" "}
-                {/* Add key prop */}
-                <Card
-                  elevation={0}
-                  key={index}
-                  sx={{
-                    padding: "1.2rem",
-                    backgroundColor: primaryColor,
-                    borderRadius: "5px",
-                  }}
-                  color="primary"
-                >
-                  <CardMedia
-                    component="img"
-                    image={`data:image/png;base64,${item?.image_str}`}
-                    sx={{ borderRadius: "5px" }}
-                    onClick={() => handleModalOpen(index)}
-                  />
-
-                  <Stack
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    spacing={3}
-                    sx={{ mt: "1rem" }}
-                  >
-                    <IconButton onClick={() => downloadImage(item.image_str)}>
-                      <DownloadTwoToneIcon />
-                    </IconButton>
-                    <IconButton>
-                      <ContentCopyIcon onClick={() => handleCopy(item)} />
-                    </IconButton>
-                    <IconButton onClick={() => deleteImage(item._id)}>
-                      <DeleteForeverTwoToneIcon />
-                    </IconButton>
-                  </Stack>
-                </Card>
-              </Grid>
-            ))}
-        </>
+        {userImages &&
+          userImages.map((item, index) => (
+            <ImageCard
+              item={item}
+              index={index}
+              variant="image"
+              onClick={() => handleModalOpen(index)}
+            />
+          ))}
       </Grid>
 
       {/* Trigger for loading more images */}
       {!loadingUserImages && userImagesPage >= 0 && (
         <div ref={loadMoreRef}></div>
       )}
+
+      {/*------ Placeholder Cards ------*/}
       <Grid
         container
         direction="row"
@@ -254,47 +151,10 @@ function MyCodes() {
         columns={8}
         spacing={3}
       >
-        <>
-          {loadingUserImages > 0 &&
-            Array.from({ length: 12 }, (_, index) => index).map((_, index) => (
-              <Grid item md={2} key={`skeleton-${index}`}>
-                <Card
-                  elevation={0}
-                  sx={{
-                    padding: "1.2rem",
-                    backgroundColor: primaryColor,
-                    borderRadius: "5px",
-                  }}
-                  color="primary"
-                >
-                  <Skeleton
-                    variant="rounded"
-                    width="100%"
-                    height="0px"
-                    animation="wave"
-                    sx={{
-                      aspectRatio: "1/1",
-                      paddingTop: "100%", // Set the height to be the same as the width
-                    }}
-                  />
-
-                  <Stack
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    spacing={3}
-                    sx={{ mt: "1rem" }}
-                  >
-                    <Skeleton variant="circular" width={30} height={30} />
-
-                    <Skeleton variant="circular" width={30} height={30} />
-
-                    <Skeleton variant="circular" width={30} height={30} />
-                  </Stack>
-                </Card>
-              </Grid>
-            ))}
-        </>
+        {loadingUserImages > 0 &&
+          Array.from({ length: 12 }, (_, index) => index).map((_, index) => (
+            <ImageCard item={_} variant="skeleton" index={index} />
+          ))}
       </Grid>
 
       {/*----------------- Modal: Image Details----------------*/}
@@ -344,32 +204,24 @@ function MyCodes() {
                 }}
               />
             </div>
-            <Stack
-              sx={{ height: "100%", padding: "2rem" }}
-              justifyContent="center"
-              alignItems="flex-start"
-            >
-              <Typography variant="subtitle2">Date created</Typography>
-              <Typography variant="body" sx={{ mb: "1rem" }}>
-                {userImages[selectedImageIndex]?.created_at != "-"
-                  ? dayjs(userImages[selectedImageIndex]?.created_at).format(
-                      "MMMM D, YYYY"
-                    )
-                  : "-"}
-              </Typography>
-              <Typography variant="subtitle2">QR Content</Typography>
-              <Typography variant="body" sx={{ mb: "1rem" }}>
-                {userImages[selectedImageIndex]?.content}
-              </Typography>
-              <Typography variant="subtitle2">Prompt</Typography>
-              <Typography variant="body" sx={{ mb: "1rem" }}>
-                {userImages[selectedImageIndex]?.prompt}
-              </Typography>
-              <Typography variant="subtitle2">Seed</Typography>
-              <Typography variant="body" sx={{ mb: "1rem" }}>
-                {userImages[selectedImageIndex]?.seed}
-              </Typography>
-            </Stack>
+            <List sx={{ height: "100%", padding: "2rem" }}>
+              <ListItemText
+                primary="Date created"
+                secondary={userImages[selectedImageIndex]?.created_at}
+              />
+              <ListItemText
+                primary="QR Content"
+                secondary={userImages[selectedImageIndex]?.content}
+              />
+              <ListItemText
+                primary="Prompt"
+                secondary={userImages[selectedImageIndex]?.prompt}
+              />
+              <ListItemText
+                primary="Seed"
+                secondary={userImages[selectedImageIndex]?.seed}
+              />
+            </List>
           </Paper>
           <IconButton
             sx={{
