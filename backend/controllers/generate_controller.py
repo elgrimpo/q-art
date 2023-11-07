92b3d4b2ad25a652003181159fa229a679cc0680
Christoph <biedermann.chris@gmail.com>
2023-11-06 19:43:06 -0800

    Use Omniinfer API to generate images

--- backend/controllers/generate_controller.py
+++ backend/controllers/generate_controller.py
@@ -1,21 +1,39 @@
 # Libraries Import
 import qrcode
 from dotenv import load_dotenv
+import os
 import requests as requests
 import json
 import copy
+from fastapi import FastAPI, HTTPException
+import httpx
+from omniinfer_client import *
 
 # App imports
-from utils.payload_config import payloadConfig
+from utils.payload_config import payloadConfig, sample_payload_config
 from controllers.images_controller import insert_image
 from utils.utils import readImage, prepare_doc
 
 
 load_dotenv()
 webui_url = "http://127.0.0.1:7860"
+omniinfer_url = "https://api.omniinfer.io/v2/txt2img"
+
+client = OmniClient(os.environ["OMNIINFER_KEY"])
+
 
 def predict(prompt, website, negative_prompt, seed, image_quality, qr_weight, user_id):
-    # Prepare QR Code Image
+
+    # Load ControlNet models
+    controlnet_model_0 = client.models().filter_by_type(ModelType.CONTROLNET).get_by_name("control_v1p_sd15_brightness.safetensors")
+    if controlnet_model_0 is None:
+        raise Exception("controlnet_0 model not found")
+
+    controlnet_model_1 = client.models().filter_by_type(ModelType.CONTROLNET).get_by_name("control_v1p_sd15_qrcode_monster_v2")
+    if controlnet_model_1 is None:
+        raise Exception("controlnet_1 model not found")
+    
+    # Create QR Code
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -27,37 +45,118 @@ def predict(prompt, website, negative_prompt, seed, image_quality, qr_weight, us
     qr_image = qr.make_image()
     qr_image.save("qrcode.png")
     image_base64_str = readImage("qrcode.png")
-    # Initial Payload
-    payload = copy.deepcopy(payloadConfig)
-    payload["prompt"] = prompt
-    payload["negative_prompt"] = negative_prompt
-    payload["seed"] = int(seed)
-    payload["alwayson_scripts"]["ControlNet"]["args"][0]["image"] = image_base64_str
-    payload["alwayson_scripts"]["ControlNet"]["args"][1]["image"] = image_base64_str
     
+    # Image Quality -> Steps
     if image_quality == "low":
-        payload["steps"] = 10
+        steps = 10
     elif image_quality == "medium":
-        payload["steps"] = 20  
+        steps = 20  
     elif image_quality == "high":
-        payload["steps"] = 30
+        steps = 30
 
-    # Translate qr_weight into guidance start, end and weight
-    qr_weight_fl = float(qr_weight)
-    payload["alwayson_scripts"]["ControlNet"]["args"][1]["guidance_start"] -= qr_weight_fl * 0.05
-    payload["alwayson_scripts"]["ControlNet"]["args"][1]["guidance_end"] += qr_weight_fl * 0.05
-    payload["alwayson_scripts"]["ControlNet"]["args"][1]["weight"] += qr_weight_fl * 0.1
-    
 
-    # Initiate Request
-    response = requests.post(url=f"{webui_url}/sdapi/v1/txt2img", json=payload)
-    api_response = response.json()
-    info = json.loads(api_response["info"])
-    image = api_response["images"][0]
+    req = Txt2ImgRequest(
+        prompt=prompt,
+        negative_prompt = negative_prompt,
+        sampler_name=Samplers.DPMPP_M_KARRAS,
+        model_name='beautifulRealistic_v40_37543.safetensors',
+        width=512,
+        height=512,
+        steps=steps,
+        batch_size=1,
+        cfg_scale=9,
+        seed= int(seed),
+        controlnet_units=[
+            ControlnetUnit(
+                input_image=image_base64_str,
+                control_mode=ControlNetMode.BALANCED,
+                model=controlnet_model_0.sd_name,
+                module=ControlNetPreprocessor.INPAINT,
+                resize_mode=ControlNetResizeMode.RESIZE_OR_CORP,
+                weight=0.35,
+                guidance_start = 0.0,
+                guidance_end = 1.0
+            )
+            ,
+            ControlnetUnit(
+                input_image=image_base64_str,
+                control_mode=ControlNetMode.BALANCED,
+                model=controlnet_model_1.sd_name,
+                module=ControlNetPreprocessor.INPAINT,
+                resize_mode=ControlNetResizeMode.RESIZE_OR_CORP,
+                weight=1.0 + float(qr_weight) * 0.1,
+                guidance_start = 0.2 - float(qr_weight) * 0.05,
+                guidance_end = 0.85 + float(qr_weight) * 0.05
+            )
+        ]
+    )
 
-    # Create database entry
-    
-    doc = prepare_doc(image, payload, info, website, image_quality, qr_weight, user_id)
-
+    res = client.sync_txt2img(req)
+    print(ProgressResponse.data)
+    if res.data.status != ProgressResponseStatusCode.SUCCESSFUL:
+        raise Exception('Failed to generate image with error: ' +
+                        res.data.failed_reason)
+    save_image(res.data.imgs_bytes[0], "qrcode-art.png")
+    image = readImage("qrcode-art.png")
+    # TODO: read info from response
+    info = {
+        "seed": -1
+    }
+    doc = prepare_doc(image, req, info, website, image_quality, qr_weight, user_id)
+    print(doc)
     inserted_image = insert_image(doc, user_id, image_quality)
     return inserted_image
+
+
+
+
+
+
+
+# def predict(prompt, website, negative_prompt, seed, image_quality, qr_weight, user_id):
+#     # Prepare QR Code Image
+#     qr = qrcode.QRCode(
+#         version=1,
+#         error_correction=qrcode.constants.ERROR_CORRECT_H,
+#         box_size=10,
+#         border=4
+#     )
+#     qr.add_data(website)  
+
+#     qr_image = qr.make_image()
+#     qr_image.save("qrcode.png")
+#     image_base64_str = readImage("qrcode.png")
+#     # Initial Payload
+#     payload = copy.deepcopy(payloadConfig)
+#     payload["prompt"] = prompt
+#     payload["negative_prompt"] = negative_prompt
+#     payload["seed"] = int(seed)
+#     payload["alwayson_scripts"]["ControlNet"]["args"][0]["image"] = image_base64_str
+#     payload["alwayson_scripts"]["ControlNet"]["args"][1]["image"] = image_base64_str
+    
+#     if image_quality == "low":
+#         payload["steps"] = 10
+#     elif image_quality == "medium":
+#         payload["steps"] = 20  
+#     elif image_quality == "high":
+#         payload["steps"] = 30
+
+#     # Translate qr_weight into guidance start, end and weight
+#     qr_weight_fl = float(qr_weight)
+#     payload["alwayson_scripts"]["ControlNet"]["args"][1]["guidance_start"] -= qr_weight_fl * 0.05
+#     payload["alwayson_scripts"]["ControlNet"]["args"][1]["guidance_end"] += qr_weight_fl * 0.05
+#     payload["alwayson_scripts"]["ControlNet"]["args"][1]["weight"] += qr_weight_fl * 0.1
+    
+
+#     # Initiate Request
+#     response = requests.post(url=f"{webui_url}/sdapi/v1/txt2img", json=payload)
+#     api_response = response.json()
+#     info = json.loads(api_response["info"])
+#     image = api_response["images"][1]
+
+#     # Create database entry
+    
+#     doc = prepare_doc(image, payload, info, website, image_quality, qr_weight, user_id)
+
+#     inserted_image = insert_image(doc, user_id, image_quality)
+#     return inserted_image
--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -13,14 +13,26 @@ def readImage(path):
     return b64img
 
 
-def prepare_doc(image, payload, info, website, image_quality, qr_weight, user_id):
-    # TODO: make user_id as part of input parameter
+def prepare_doc(image, req, info, website, image_quality, qr_weight, user_id):
+    
+    
+    sampler_name = req.sampler_name
+    control_mode_0 = req.controlnet_units[0].control_mode.value
+    model_0 = req.controlnet_units[0].model
+    module_0 = req.controlnet_units[0].module.value
+    resize_mode_0 = req.controlnet_units[0].resize_mode.value
+
+    control_mode_1 = req.controlnet_units[1].control_mode.value
+    model_1 = req.controlnet_units[1].model
+    module_1 = req.controlnet_units[1].module.value
+    resize_mode_1 = req.controlnet_units[1].resize_mode.value
+
     doc = {
         "user_id": user_id,
         "created_at": datetime.datetime.utcnow(),
         "image_str": image,
-        "prompt": payload["prompt"],
-        "negative_prompt": payload["negative_prompt"],
+        "prompt": req.prompt,
+        "negative_prompt": req.negative_prompt,
         "content": website,
         "presets": [""],
         "seed": info["seed"],
@@ -29,42 +41,26 @@ def prepare_doc(image, payload, info, website, image_quality, qr_weight, user_id
         "width": "512",
         "height": "512",
         "query_type": "txt2img",
-        "steps": payload["steps"],
-        "cfg_scale": payload["cfg_scale"], 
-        "sampler_name": payload["sampler_name"],
+        "steps": req.steps,
+        "cfg_scale": req.cfg_scale, 
+        "sampler_name": sampler_name,
         "controlnet0": {
-            "control_mode": payload["alwayson_scripts"]["ControlNet"]["args"][0][
-                "control_mode"
-            ],
-            "model": payload["alwayson_scripts"]["ControlNet"]["args"][0]["model"],
-            "module": payload["alwayson_scripts"]["ControlNet"]["args"][0]["module"],
-            "weight": payload["alwayson_scripts"]["ControlNet"]["args"][0]["weight"],
-            "guidance_start": payload["alwayson_scripts"]["ControlNet"]["args"][0][
-                "guidance_start"
-            ],
-            "guidance_end": payload["alwayson_scripts"]["ControlNet"]["args"][0][
-                "guidance_end"
-            ],
-            "resize_mode": payload["alwayson_scripts"]["ControlNet"]["args"][0][
-                "resize_mode"
-            ],
+            "control_mode": control_mode_0,
+            "model": model_0,
+            "module": module_0,
+            "weight": req.controlnet_units[0].weight,
+            "guidance_start": req.controlnet_units[0].guidance_start,
+            "guidance_end": req.controlnet_units[0].guidance_end,
+            "resize_mode": resize_mode_0,
         },
         "controlnet1": {
-            "control_mode": payload["alwayson_scripts"]["ControlNet"]["args"][1][
-                "control_mode"
-            ],
-            "model": payload["alwayson_scripts"]["ControlNet"]["args"][1]["model"],
-            "module": payload["alwayson_scripts"]["ControlNet"]["args"][1]["module"],
-            "weight": payload["alwayson_scripts"]["ControlNet"]["args"][1]["weight"],
-            "guidance_start": payload["alwayson_scripts"]["ControlNet"]["args"][1][
-                "guidance_start"
-            ],
-            "guidance_end": payload["alwayson_scripts"]["ControlNet"]["args"][1][
-                "guidance_end"
-            ],
-            "resize_mode": payload["alwayson_scripts"]["ControlNet"]["args"][1][
-                "resize_mode"
-            ],
+            "control_mode": control_mode_1,
+            "model": model_1,
+            "module": module_1,
+            "weight": req.controlnet_units[0].weight,
+            "guidance_start": req.controlnet_units[0].guidance_start,
+            "guidance_end": req.controlnet_units[0].guidance_end,
+            "resize_mode": resize_mode_1,
         },
     }
     return doc
