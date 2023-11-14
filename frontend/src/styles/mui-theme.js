80fe0247d32cee93ab81e66c30bbca4a261bf357
Christoph <biedermann.chris@gmail.com>
2023-11-14 10:40:54 -0800

    Adjust selected color of bottom navigation

--- frontend/src/styles/mui-theme.js
+++ frontend/src/styles/mui-theme.js
@@ -1,81 +1,82 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const palette = {
-    primary: {
-        main:'#70E195',
-        light: '#c5f2d2',
-        dark: '#00ac4f'
-    },
-    secondary: {
-        main: '#333333',
-        light: '#8b8b8b',
-        dark: '#000000'
-    }
-}
-
+  primary: {
+    main: "#70E195",
+    light: "#c5f2d2",
+    dark: "#00ac4f",
+  },
+  secondary: {
+    main: "#333333",
+    light: "#8b8b8b",
+    dark: "#000000",
+  },
+};
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: palette.primary.main,
-            light: palette.primary.light,
-            dark: palette.primary.dark
-        },
-        secondary: {
-            main: palette.secondary.main,
-            light: palette.secondary.light,
-            dark: palette.secondary.dark
-        }
-
+  palette: {
+    primary: {
+      main: palette.primary.main,
+      light: palette.primary.light,
+      dark: palette.primary.dark,
     },
-    typography: {
-        fontWeightMedium: 700, 
-        fontWeightBold: 900,
-        h5: {
+    secondary: {
+      main: palette.secondary.main,
+      light: palette.secondary.light,
+      dark: palette.secondary.dark,
+    },
+  },
+  typography: {
+    fontWeightMedium: 700,
+    fontWeightBold: 900,
+    h5: {
+      fontFamily: "Roboto Serif",
+      fontWeight: 900,
+      fontStyle: "italic",
+    },
+  },
+  components: {
+    MuiBottomNavigationAction: {
+        styleOverrides: {
+        root: {
+          '&.Mui-selected': {
+            color: palette.primary.dark, 
+          },
+        },
+      }},
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          fontFamily: "Roboto Serif",
+          fontWeight: 500,
+          fontStyle: "italic",
+          fontSize: "1.2rem",
+          margin: "0.5rem 1rem",
+          padding: "0px",
+          "&.Mui-selected": {
             fontFamily: "Roboto Serif",
             fontWeight: 900,
-            fontStyle: 'italic',
-        }
+            fontStyle: "italic",
+            color: palette.primary.dark,
+          },
+        },
       },
-    components: {
-        MuiTab: {
-            styleOverrides: {
-                root: {
-                    fontFamily: "Roboto Serif",
-                    fontWeight: 500,
-                    fontStyle: 'italic',
-                    fontSize: '1.2rem',
-                    margin: '0.5rem',
-                    padding: "0px",
-                    '&.Mui-selected': {
-                        
-                                fontFamily: "Roboto Serif",
-                                fontWeight: 900,
-                                fontStyle: 'italic',   
-                                color: palette.primary.dark,
-                            },
-                                
-                        },
-                    
-                    }
-                    
-                },
-                MuiTabs: {
-                    styleOverrides: {
-                      indicator: {
-                        backgroundColor: 'rgba(0, 0, 0, 0);' 
-                      }
-                    }
-                  },
-                  MuiTabPanel: {
-                    styleOverrides: {
-                        root: {
-                            padding: "0px"
-                        }}}
-            }
-        
-    
+    },
+    MuiTabs: {
+      styleOverrides: {
+        indicator: {
+          backgroundColor: "rgba(0, 0, 0, 0);",
+        },
+      },
+    },
+    MuiTabPanel: {
+      styleOverrides: {
+        root: {
+          padding: "0px",
+        },
+      },
+    },
+  },
+});
 
-})
-
-export default theme
+export default theme;
