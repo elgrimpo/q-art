816f553c1b0bbc17ed81a5189937d1c883c7c599
Christoph <biedermann.chris@gmail.com>
2023-11-28 10:56:44 -0800

    Separate component for NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 // Libraries imports
 import * as React from "react";
-import { Routes, Route, Navigate, Link, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import {
-  Tab,
-  Tabs,
   Toolbar,
   Box,
   Button,
   Snackbar,
   Alert,
   IconButton,
-  BottomNavigation,
-  BottomNavigationAction,
 } from "@mui/material";
-import AutoFixHighTwoToneIcon from "@mui/icons-material/AutoFixHighTwoTone";
-import ImageTwoToneIcon from "@mui/icons-material/ImageTwoTone";
-import Diversity1TwoToneIcon from "@mui/icons-material/Diversity1TwoTone";
 import CloseIcon from "@mui/icons-material/Close";
 import { useEffect } from "react";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import theme from "./styles/mui-theme";
 
 // App imports
@@ -31,6 +23,7 @@ import AccountMenu from "./pages/Home/AccountMenu";
 import ImageGallery from "./pages/MyCodes/ImageGallery";
 import { useUtils } from "./utils/utils";
 import Account from "./pages/Home/Account";
+import NavBar from "./NavBar";
 
 /* -------------------------------------------------------------------------- */
 /*                               COMPONENT START                              */
@@ -52,33 +45,19 @@ function App() {
   // Utils functions
   const { getUserInfo, logout, closeAlert } = useUtils();
 
-  // Screen size
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-
-  // URL Path
-  const location = useLocation();
-
-  // Current Tab
-  const [tabValue, setTabValue] = React.useState("Generate");
-
   /* -------------------------------- FUNCTIONS ------------------------------- */
 
   // Login
   const handleLogin = async () => {
-    window.open(`${process.env.REACT_APP_BACKEND_URL}/api/login/google`, "_self");
-  }
+    window.open(
+      `${process.env.REACT_APP_BACKEND_URL}/api/login/google`,
+      "_self"
+    );
+  };
   // Check if user session exists
   useEffect(() => {
     getUserInfo();
   }, [generatedImage, userImages, getUserInfo]);
-
-  
-  // Change selected Tab based on URL path
-  useEffect(() => {
-    const { pathname } = location;
-
-    setTabValue(pathname);
-  }, [location]);
 
   /* -------------------------------------------------------------------------- */
   /*                              COMPONENT RENDER                              */
@@ -95,30 +74,7 @@ function App() {
 
           {/* TABS */}
           <Box className="menu-container">
-            <Tabs
-              value={tabValue}
-              sx={{ display: { xs: "none", md: "flex" } }}
-              centered
-            >
-              <Tab
-                component={Link}
-                label="Generate"
-                value="/generate"
-                to="/generate"
-              />
-              <Tab
-                component={Link}
-                label="My codes"
-                value="/mycodes"
-                to="/mycodes"
-              />
-              <Tab
-                component={Link}
-                label="Explore"
-                value="/explore"
-                to="/explore"
-              />
-            </Tabs>
+            <NavBar />
           </Box>
 
           {/* ACCOUNT */}
@@ -141,21 +97,13 @@ function App() {
             {/* MY CODES PAGE */}
             <Route
               path="mycodes"
-              element={
-                <ImageGallery
-                  imageType="userImages"
-                />
-              }
+              element={<ImageGallery imageType="userImages" />}
             />
 
             {/* EXPLORE PAGE */}
             <Route
               path="explore"
-              element={
-                <ImageGallery
-                  imageType="communityImages"
-                />
-              }
+              element={<ImageGallery imageType="communityImages" />}
             />
 
             {/* ACCOUNT */}
@@ -188,43 +136,6 @@ function App() {
             {alertMessage}
           </Alert>
         </Snackbar>
-
-        {/* ----------------------- BOTTOM NAVIGATION ------------------- */}
-        {isMobile && (
-          <BottomNavigation
-            showLabels
-            value={tabValue}
-            onChange={(event, newValue) => {
-            }}
-          >
-            {/* GENERATE PAGE */}
-            <BottomNavigationAction
-              component={Link}
-              value="Generate"
-              label="Generate"
-              to="/generate"
-              icon={<AutoFixHighTwoToneIcon />}
-            />
-
-            {/* MY CODES PAGE */}
-            <BottomNavigationAction
-              component={Link}
-              value="My codes"
-              label="My codes"
-              to="/mycodes"
-              icon={<ImageTwoToneIcon />}
-            />
-
-            {/* EXPLORE PAGE */}
-            <BottomNavigationAction
-              component={Link}
-              value="Explore"
-              label="Explore"
-              to="/explore"
-              icon={<Diversity1TwoToneIcon />}
-            />
-          </BottomNavigation>
-        )}
       </div>
     </ThemeProvider>
   );
