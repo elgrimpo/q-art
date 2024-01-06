842164e0282d2404439c5433cb4231ba54200792
Christoph <biedermann.chris@gmail.com>
2024-01-06 10:00:05 +0100

    update styles

--- frontend/src/utils/ImageStyles.js
+++ frontend/src/utils/ImageStyles.js
@@ -1,104 +1,136 @@
-export const styles = [{
-  id: 1,
-  title: "Default",
-  prompt: "",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6575fc6828c914471b835383.png",
-  keywords: [],
-  sd_model: "colorful_v31_62333.safetensors"
+export const styles = [
+  {
+    id: 1,
+    title: "Default",
+    prompt: "",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6575fc6828c914471b835383.png",
+    keywords: [],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 2,
+    title: "Ukiyo-e",
+    prompt:
+      "Detailed, Graphic Novel, Cinematic, Ukiyo-e Flat Design, Dramatic, Scene, Establishing Shot, Proportion",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574d4189961d5a54e2ff50e.png",
+    keywords: ["Flat Design", "Ukiyo-e"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 3,
+    title: "Expressionism",
+    prompt:
+      "abstract expressionist painting, award-winning photo, energetic brushwork, bold colors, abstract forms, expressive",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574d4b69961d5a54e2ff50f.png",
+    keywords: ["Expressionism", "Bold colors"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 4,
+    title: "Dreamy",
+    prompt:
+      "digital painting, extremely smooth, fluid, 3d fractals, light particles, dreamy, shimmering, dreamy glow, HQ, 4K, <lora:LAS_17554:0.7>",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574d5f69961d5a54e2ff514.png",
+    keywords: ["Dreamy glow", "Light particles"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 5,
+    title: "Low Poly Art",
+    prompt:
+      "Low-Poly Art, Origami, Painting By Salvador Dali, Scene, Dramatic, Cinematic, Establishing Shot, 4k, UHD",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574d6939961d5a54e2ff517.png",
+    keywords: ["Origami", "Low-Poly"],
+    sd_model: "epicrealism_pureEvolutionV5_97793.safetensors",
+  },
+  {
+    id: 6,
+    title: "Photography",
+    prompt:
+      "Photography, Happy Colors, Epic Composition, Cinematic, Detailed, 4k",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574d75a9961d5a54e2ff519.png",
+    keywords: ["Photography", "Cinematic"],
+    sd_model: "cyberrealistic_v40_151857.safetensors",
+  },
+  {
+    id: 7,
+    title: "Ink",
+    prompt:
+      "<lora:zyd232_InkStyle_v1_0_53697:1>, monochrome, ink sketch, watercolors, brush strokes",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574d88f9961d5a54e2ff51a.png",
+    keywords: ["Ink sketch", "monochrome"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 9,
+    title: "Oil Painting",
+    prompt:
+      "<lora:bichu-v0612_65240:0.6>, oil painting, masterpiece, best quality",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/sushi_oilpainting_style.png",
+    keywords: ["80s style", "CMYK Colors"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
 
-},
-{
-  id: 2,
-  title: "Ukiyo-e",
-  prompt: "Detailed, Graphic Novel, Cinematic, Ukiyo-e Flat Design, Dramatic, Scene, Establishing Shot, Proportion",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574d4189961d5a54e2ff50e.png",
-  keywords: ["Flat Design", "Ukiyo-e"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-
-}, {
-  id: 3,
-  title: "Expressionism",
-  prompt: "abstract expressionist painting, award-winning photo, energetic brushwork, bold colors, abstract forms, expressive",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574d4b69961d5a54e2ff50f.png",
-  keywords: ["Expressionism", "Bold colors"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-}, {
-  id: 4,
-  title: "Dreamy",
-  prompt: "digital painting, extremely smooth, fluid, 3d fractals, light particles, dreamy, shimmering, dreamy glow, HQ, 4K",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574d5f69961d5a54e2ff514.png",
-  keywords: ["Dreamy glow", "Light particles"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-
-},{
-  id: 5,
-  title: "Low Poly Art",
-  prompt: "Low-Poly Art, Origami, Painting By Salvador Dali, Scene, Dramatic, Cinematic, Establishing Shot, 4k, UHD",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574d6939961d5a54e2ff517.png",
-  keywords: ["Origami", "Low-Poly"],
-  sd_model: "epicrealism_pureEvolutionV5_97793.safetensors"
-
-}, {
-  id: 6,
-  title: "Photography",
-  prompt: "Photography, Happy Colors, Epic Composition, Cinematic, Detailed, 4k",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574d75a9961d5a54e2ff519.png",
-  keywords: ["Photography", "Cinematic"],
-  sd_model: "cyberrealistic_v40_151857.safetensors"
-
-
-},{
-  id: 7,
-  title: "Ink",
-  prompt: "<lora:zyd232_InkStyle_v1_0_53697:1>, monochrome, ink sketch, watercolors, brush strokes",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574d88f9961d5a54e2ff51a.png",
-  keywords: ["Ink sketch", "monochrome"],
-  sd_model: "colorful_v31_62333.safetensors"
-}, {
-  id: 8,
-  title: "80s",
-  prompt: "80s style, cmyk colors, vaporwave, atari game style",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/65919ed63271f991a259e7a6.png",  
-  keywords: ["80s style", "CMYK Colors"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-
-}, {
-  id: 9,
-  title: "Japonism",
-  prompt: "Japonism, Outlined, Closeup-View",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574e2af9961d5a54e2ff51e.png",  
-  keywords: ["Japonism", "Kitsch"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-
-},{
-  id: 10,
-  title: "Vector Art",
-  prompt: "Flat Design, Vector Art, illustrator",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6576499b56ae6bad355304cc.png",  
-  keywords: ["Flat Design", "Illustrator"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-}, {
-  id: 11,
-  title: "Sticker",
-  prompt: "Art Nouveau, Sticker, Turkish-Style, Miniature Faking",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/6574e5ae9961d5a54e2ff525.png",  
-  keywords: ["Miniature Faking", "Art Nouveau"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-
-},{
-  id: 12,
-  title: "Rainbow",
-  prompt: "Abstract, Rainbowcore, Color Blend",
-  image_url: "https://qrartimages.s3.us-west-1.amazonaws.com/657619f756ae6bad355304bf.png",  
-  keywords: ["Rainbow Core", "Color Blend"],
-  sd_model: "colorful_v31_62333.safetensors"
-
-}, 
-]
+  {
+    id: 11,
+    title: "Chinese art",
+    prompt: "Chinese traditional art, <lora:wuxia2_62008:0.8>, <lora:MoXinV1_12781:0.4>",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6598378e55848e0542b40d0a.png",
+    keywords: ["Japonism", "Kitsch"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 11,
+    title: "Watercolor",
+    prompt: "Watercolor painting, soft colors, <lora:Colorwater_v4:0.5, masterpiece, sharp>",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/659836b555848e0542b40d07.png",
+    keywords: ["Water color"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 12,
+    title: "Vector Art",
+    prompt: "Flat Design, Vector Art, illustrator",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6576499b56ae6bad355304cc.png",
+    keywords: ["Flat Design", "Illustrator"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 13,
+    title: "Sticker",
+    prompt: "Art Nouveau, Sticker, Turkish-Style, Miniature Faking",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/6574e5ae9961d5a54e2ff525.png",
+    keywords: ["Miniature Faking", "Art Nouveau"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 16,
+    title: "Color blend",
+    prompt: "Synesthesia, Color Blend, streams of colored paint",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/659809b555848e0542b40ce5.png",
+    keywords: ["Rainbow Core", "Color Blend"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+  {
+    id: 10,
+    title: "80s",
+    prompt: "cmyk colors, vaporwave",
+    image_url:
+      "https://qrartimages.s3.us-west-1.amazonaws.com/65919ed63271f991a259e7a6.png",
+    keywords: ["80s style", "CMYK Colors"],
+    sd_model: "colorful_v31_62333.safetensors",
+  },
+];
