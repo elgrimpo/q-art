d972fbc0e38f5b731ba313b851a707be964b0d09
Christoph <biedermann.chris@gmail.com>
2024-02-20 16:01:53 -0800

    add env arguments to next.config.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL
+  },
     async rewrites() {
         return [
           {
