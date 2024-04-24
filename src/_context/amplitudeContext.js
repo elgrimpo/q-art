409536a170f0c41546416a0a3e75764afb022fec
Christoph <biedermann.chris@gmail.com>
2024-04-24 12:05:13 -0700

    add amplitude api key

--- src/_context/amplitudeContext.js
+++ src/_context/amplitudeContext.js
@@ -3,12 +3,13 @@
 import { useEffect, createContext } from "react";
 import { init, track } from "@amplitude/analytics-browser";
 
-const AMPLITUDE_API_KEY = process.env.AMPLITUDE_API_KEY;
+const AMPLITUDE_API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
 
 export const AmplitudeContext = createContext({});
 
 const AmplitudeContextProvider = ({ children }) => {
   useEffect(() => {
+    console.log("amplitude init")
     init(AMPLITUDE_API_KEY, undefined, {
       defaultTracking: {
         sessions: true,
