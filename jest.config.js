b9fb81376924beb28f3066140bd3a630c5fef9a3
Christoph <biedermann.chris@gmail.com>
2026-06-11 23:39:30 -0700

    test(frontend): install @testing-library and configure jest-dom setup

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const createJestConfig = nextJest({ dir: './' })
 module.exports = createJestConfig({
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['<rootDir>/src/__tests__/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/jest.setup.js'],
   moduleNameMapper: {
     // Force jose to use the Node CJS build (jsdom triggers browser export condition)
     '^jose(.*)$': '<rootDir>/node_modules/jose/dist/node/cjs$1',
